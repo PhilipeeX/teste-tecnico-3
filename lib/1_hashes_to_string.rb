465f9872872501585d78b1fb203cec2fc8de1966
PhilipeeX <dev.philipe@outlook.com>
2023-08-24 13:03:33 -0300

    refactor(1_hashes to string): algorithm refactored for a better readability

--- lib/1_hashes_to_string.rb
+++ lib/1_hashes_to_string.rb
@@ -1,19 +1,5 @@
 def solucao(arr)
-  output = []
-  arr.each do |hash|
-    output << hash.values.map { |value| value.to_s[0,11].ljust(11) }
-    output << ("\n") unless arr[arr.length - 1] == hash
-  end
-  output.join
+  arr.map do |hash|
+    hash.values.map { |value| value.to_s[0,11].ljust(11) }.join
+  end.join("\n")
 end
-
-p solucao([
-          { name: 'Maria Neusa de Aparecida',
-            cpf: '97905796671',
-            state: 'Sao Paulo',
-            value: '1234' },
-          { name: 'Ricardo Fontes',
-            cpf: '44010762900',
-            state: 'Rio Grande do Sul',
-            value: '567' }
-        ])
